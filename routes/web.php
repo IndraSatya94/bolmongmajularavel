ad9d0784230543799add8d04a0dad8c52196fcf4
IndraSatya94 <minsatya94@gmail.com>
2021-05-16 22:08:46 +0700

    new

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('template/index');
+});
+
+Route::get('/dinas', function () {
+    return view('template/detail/dinas');
+});
+
+Route::get('/login', function () {
+    return view('pengguna/login');
+});
+
+Route::post('/postlogin', 'LoginController@postlogin')->name('postlogin');
